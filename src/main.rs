e737091c4b0b020347830932a6172c376114be32
ShadowChild <shadychild12@gmail.com>
2020-09-24 15:32:09 +0100

    Very simple wrapper over the process

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use clap::{Arg, App};
+use std::process::{Command, Stdio};
+use std::io::{BufReader, Error, ErrorKind, BufRead};
 
-fn main() {
+fn main() -> Result<(), Error> {
 
+    // Define the jar file, as we use it multiple times
+    let candor_jar = "CandorManager-snapshot.jar";
+
+    // Ask clap to parse our command line, and return a set of matches.
     let matches = App::new("CandorStart")
         .version("1.0")
         .author("Innoxium Tech <innoxium.co.uk>")
@@ -19,4 +25,28 @@ fn main() {
 
         println!("{}", matches.value_of("url").unwrap());
     }
+
+    // Create the candor process.
+    let child = Command::new("java")
+        .stdout(Stdio::piped())
+        .arg(format!("{}{}", "-javaagent:", candor_jar))
+        .arg("-jar")
+        .arg(candor_jar)
+        .arg("url")
+        .arg(matches.value_of("url").unwrap())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Could not create std out"))?;
+
+    // Create reader for us to pipe in to our own output
+    let reader = BufReader::new(child);
+
+    // Now we can funnel the output in to our stdout.
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("{}", line));
+
+    // Return either the error, or ok
+    Ok(())
 }
